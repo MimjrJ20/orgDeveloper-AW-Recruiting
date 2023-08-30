5b8817909d06839bd2cfddba712c1df0ea4ca245
Michele Rosa <michele.jeniffer.mr@gmail.com>
2023-08-30 18:06:50 -0300

    modal proposal

--- force-app/main/default/lwc/candidateProposeModal/candidateProposeModal.js
+++ force-app/main/default/lwc/candidateProposeModal/candidateProposeModal.js
@@ -1,28 +1,43 @@
-import { api} from "lwc";
+import { api, track } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import LightningModal from "lightning/modal";
 import searchJobByIdCandidateAndInterview from "@salesforce/apex/JobApplicationDAO.searchJobByIdCandidateAndInterview";
-
-
 
 export default class CandidateProposeModal extends LightningModal{
 
     @api idCandidate; 
-    value;
-    options = [];
+
+    @track value;
+    @track options = [];
+
+    @track name;
+    @track firstName;
+    @track lastName;
+    @track stateProvince;
+    @track country;
+
+    @track skills;
+    @track positionName;
+    @track department;
+    @track accountName;
+    @track description;
+
 
     //função para gerar o pdf
-    handleGeneratePdfClick() {
+    handleGeneratePdfClick() {;
 
-        const urlId = this.extractIdFromUrl(window.location.href);
-
-        window.open('/apex/CandidatePDF?idCandidate=' + urlId);
-
-        this.close("cancel");
+        const visualforceUrl = `/apex/CandidatePDF?idCandidate=${this.idCandidate}
+                                &name=${this.name}&firstName=${this.firstName}&lastName=${this.lastName}&skills=${this.skills}
+                                &stateProvince=${this.stateProvince}&country=${this.country}
+                                &positionName=${this.positionName}&description=${this.description}
+                                &accountName=${this.accountName}&department=${this.department}`;
+        window.open(visualforceUrl, "_blank");
+        this.close("okay");
     }
 
     //função para fechar o modal
     handleNoOkay() {
-        this.close("cancel");
+        this.close("okay");
     }
 
     //função para extrair o ID do candidato da UR
@@ -57,9 +72,23 @@ export default class CandidateProposeModal extends LightningModal{
 
             .then((result) => {
 
+                if (result && result.length > 0) {
+                    const candidate = result[0].Candidate__r; 
+                    this.name = candidate.Name;
+                    this.firstName = candidate.First_Name__c != null ? candidate.First_Name__c : "Candidate";
+                    this.lastName = candidate.Last_Name__c != null ? candidate.Last_Name__c : "";
+                    this.stateProvince = candidate.State_Province__c != null ? candidate.State_Province__c : "São Paulo";
+                    this.country = candidate.Country_List__c != null ? candidate.Country_List__c : "BR - Brazil";
+                }
+
                 this.options = result.map(job => ({
                     label: job.Name + " - " + job.Position__r.Name,
-                    value: job.Id
+                    value: job.Id,
+                    jobPositionName: job.Position__r.Name,
+                    jobSkills: job.Position__r.Skills_Required__c,
+                    jobDepartment: job.Position__r.Department__c,
+                    jobAccountName: job.Position__r.Account__r.Name,
+                    jobDescription: job.Position__r.Job_Description__c
                 
                 }));
             })
@@ -68,14 +97,25 @@ export default class CandidateProposeModal extends LightningModal{
 
                 console.log(error);
             });
-
-            
     }
 
-    // rastrear qual opção foi selecionada
+    //rastrear qual opção foi selecionada
     handleChange(event) {
+
         this.value = event.detail.value;
         console.log("valor selecionado : " + this.value);
+
+        //buscar o job selecionado com base no this.value
+        const selectedJob = this.options.find(job => job.value === this.value);
+
+        if (selectedJob) {
+
+            this.positionName = selectedJob.jobPositionName;
+            this.skills = selectedJob.jobSkills != null ? selectedJob.jobSkills : "ALL";
+            this.department = selectedJob.jobDepartment != null ? selectedJob.jobDepartment : "TBD";
+            this.accountName = selectedJob.jobAccountName != null ? selectedJob.jobAccountName : "TBD";
+            this.description = selectedJob.jobDescription != null ? selectedJob.jobDescription : "TBD";
+        }
     }
 
 
